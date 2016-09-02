d29d369bc1264d63ad9b50d124923b9d86882366
Kaiwen Sun <skw_kevin@126.com>
2016-09-02 22:14:41 +0800

    Create 105.Construct Binary Tree from Preorder and Inorder Traversal.java

---
+++ 105.Construct Binary Tree from Preorder and Inorder Traversal.java
@@ -0,0 +1,40 @@
+/**
+ *Basic idea:
+ * Determine the range of subtree arrays, and solve recursivelly.
+ *Result:
+ * 202 / 202 test cases passed.
+ * Status: Accepted
+ * Runtime: 21 ms
+ * Your runtime beats 21.54% of javasubmissions.
+ *Date:
+ * 9/2/2016
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        return buildTree(preorder,0,preorder.length,inorder,0,inorder.length);
+    }
+    private TreeNode buildTree(int[] preorder, int pl, int pr, int[] inorder, int il, int ir){
+        
+        for(int i=il;i<ir;i++){
+            if(inorder[i]==preorder[pl]){
+                int lsize = i-il;
+                TreeNode ltree = buildTree(preorder,pl+1,pl+1+lsize,inorder,il,il+lsize);
+                TreeNode rtree = buildTree(preorder,pl+1+lsize,pr,inorder, il+1+lsize,ir);
+                TreeNode root = new TreeNode(preorder[pl]);
+                root.left = ltree;
+                root.right = rtree;
+                return root;
+            }
+        }
+        return null;
+    }
+}
