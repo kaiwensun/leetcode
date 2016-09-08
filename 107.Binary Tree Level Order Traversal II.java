f11322a8f0ba81652f03cf795c4360c0fd275c82
Kaiwen Sun <skw_kevin@126.com>
2016-09-08 16:07:31 +0800

    Create 107.Binary Tree Level Order Traversal II.java

---
+++ 107.Binary Tree Level Order Traversal II.java
@@ -0,0 +1,38 @@
+/**
+ *Basic idea:
+ * DFS
+ *Result:
+ * 34 / 34 test cases passed.
+ * Status: Accepted
+ * Runtime: 2 ms
+ * We are in the progress of updating the graph distribution. Please check the distribution again within weeks.
+ *Date:
+ * 9/8/2016
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    private List<List<Integer>> res;
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        res = new LinkedList<List<Integer>>();
+        dfs(root,0);
+        return res;
+    }
+    private void dfs(TreeNode root, int level){
+        if(root==null)
+            return;
+        if(level==res.size())
+            res.add(0,new LinkedList<Integer>());
+        List<Integer> row = res.get(res.size()-level-1);
+        row.add(root.val);
+        dfs(root.left,level+1);
+        dfs(root.right,level+1);
+    }
+}
