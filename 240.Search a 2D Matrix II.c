62144994176981c554c29ba9d9e2230e95b1efb7
Kaiwen Sun <skw_kevin@126.com>
2016-09-21 23:24:21 -0700

    Create 240.Search a 2D Matrix II.c

---
+++ 240.Search a 2D Matrix II.c
@@ -0,0 +1,25 @@
+/**
+ *Basic idea:
+ * search along diagnal. each comparation excludes either one row or one colume.
+ *Result:
+ * 127 / 127 test cases passed.
+ * Status: Accepted
+ * Runtime: 52 ms
+ * Your runtime beats 94.44% of c submissions.
+ *Date:
+ * 9/19/2016
+ */
+bool searchMatrix(int** matrix, int matrixRowSize, int matrixColSize, int target) {
+    int c = 0;
+    int r = matrixRowSize-1;
+    while(r>=0 && c<matrixColSize){
+        int t = matrix[r][c];
+        if(t==target)
+            return true;
+        if(t>target)
+            r--;
+        else
+            c++;
+    }
+    return false;
+}
