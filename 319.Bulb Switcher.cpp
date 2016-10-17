f10e983b8b4cb69bc83048a4ca613858373fa8ff
Kaiwen Sun <skw_kevin@126.com>
2016-10-16 23:23:05 -0700

    Create 319.Bulb Switcher.cpp

---
+++ 319.Bulb Switcher.cpp
@@ -0,0 +1,15 @@
+/**
+ *Result:
+ * 35 / 35 test cases passed.
+ * Status: Accepted
+ * Runtime: 0 ms
+ * Your runtime beats 6.19% of cpp submissions.
+ *Date:
+ * 10/16/2016
+ */
+class Solution {
+public:
+    int bulbSwitch(int n) {
+        return (int)sqrt(n);
+    }
+};
