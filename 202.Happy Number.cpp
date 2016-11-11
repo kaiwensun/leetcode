f30d132e90010a3abae79dbc46857bddf03debbd
Kaiwen Sun <skw_kevin@126.com>
2016-11-11 00:09:25 -0800

    Create 202.Happy Number.cpp

---
+++ 202.Happy Number.cpp
@@ -0,0 +1,36 @@
+/**
+ *Result:
+ * 401 / 401 test cases passed.
+ * Status: Accepted
+ * Runtime: 0 ms
+ * Your runtime beats 85.31% of cpp submissions.
+ */
+class Solution {
+public:
+    bool isHappy(int n) {
+        bool visited[193] = {false};
+        while(n!=1){
+            if(n>=193){
+                int tmpn = 0;
+                while(n!=0){
+                    tmpn+=(n%10)*(n%10);
+                    n/=10;
+                }
+                n = tmpn;
+            }else{
+                if(visited[n]){
+                    return false;
+                }else{
+                    visited[n] = true;
+                    int tmpn = 0;
+                    while(n!=0){
+                        tmpn+=(n%10)*(n%10);
+                        n/=10;
+                    }
+                    n = tmpn;
+                }
+            }
+        }
+        return true;
+    }
+};
