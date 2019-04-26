0c3c91a9b3576053d296a34087e589df81e8cbf4
Kaiwen Sun <skw_kevin@126.com>
2019-04-26 16:15:19 -0700

    Create 853.Car Fleet.py

---
+++ 853.Car Fleet.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def carFleet(self, target, position, speed):
+        """
+        :type target: int
+        :type position: List[int]
+        :type speed: List[int]
+        :rtype: int
+        """
+        posi_speed_list = sorted(zip(position, speed))
+        time = 0
+        res = 0
+        for posi, speed in posi_speed_list[::-1]:
+            ideal_time = (target - posi) / float(speed)
+            if ideal_time <= time:
+                continue
+            else:
+                res += 1
+                time = ideal_time
+        return res
