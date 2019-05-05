8823d049eb4cd1794f0469924ce8b4bb246ad9d1
Kaiwen Sun <skw_kevin@126.com>
2019-05-04 23:53:38 -0700

    Create 1039.Minimum Score Triangulation of Polygon.py

---
+++ 1039.Minimum Score Triangulation of Polygon.py
@@ -0,0 +1,19 @@
+import functools
+class Solution(object):
+    
+    def minScoreTriangulation(self, A):
+        self.A = A
+        return self.helper(0, len(A) - 1)
+        
+    @functools.lru_cache(None)
+    def helper(self, i, j):
+        A = self.A
+        if j - i == 2:
+            return A[i] * A[i + 1] * A[i + 2]
+        if j - i < 2:
+            return 0
+        # res = A[i] * A[j - 1] * A[j] + self.helper(i, j - 1)
+        res = float('inf')
+        for k in range(i + 1, j):
+            res = min(res, self.helper(i, k) + self.helper(k, j) + A[i] * A[k] * A[j])
+        return res
