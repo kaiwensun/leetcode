8ab065b973b7461e4735527c27ddcb7108a0ba8e
Kaiwen Sun <skw_kevin@126.com>
2019-05-18 21:14:45 -0700

    Update 1049.Last Stone Weight II.py

--- 1049.Last Stone Weight II.py
+++ 1049.Last Stone Weight II.py
@@ -1,7 +1,6 @@
 import functools
 class Solution:
     def lastStoneWeightII(self, stones: List[int]) -> int:
-        mx_w = sum(stones)
         @functools.lru_cache(None)
         def dp(i, diff):
             if i == 0:
@@ -9,7 +8,7 @@ class Solution:
             me = stones[i]
             return dp(i - 1, abs(abs(diff - me))) or dp(i - 1, diff + me)
 
-        for possible_diff in range(mx_w + 1):
+        for possible_diff in range(sum(stones) + 1):
             if dp(len(stones) - 1, possible_diff):
                 return possible_diff
         assert(0)
