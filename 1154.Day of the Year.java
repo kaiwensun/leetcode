b573661315e5497e2137aa16ebe5839e1250bbfe
Kaiwen Sun <skw_kevin@126.com>
2019-08-12 22:33:09 -0700

    Create 1154.Day of the Year.java

---
+++ 1154.Day of the Year.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int dayOfYear(String date) {
+        int year = Integer.parseInt(date.substring(0, 4));
+        int month = Integer.parseInt(date.substring(5, 7));
+        int day = Integer.parseInt(date.substring(8, 10));
+        int[] days = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+            days[1] = 29;
+        }
+        int res = 0;
+        for (int m = 1; m < month; m++) {
+            res += days[m - 1];
+        }
+        res += day;
+        return res;
+    }
+}
