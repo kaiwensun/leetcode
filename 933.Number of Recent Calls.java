289240545c117fb39939c3ca1eef6aa2c810fd07
Kaiwen Sun <skw_kevin@126.com>
2019-08-25 16:18:42 -0700

    Create 933.Number of Recent Calls.java

---
+++ 933.Number of Recent Calls.java
@@ -0,0 +1,21 @@
+class RecentCounter {
+
+    Queue<Integer> queue = new LinkedList<>();
+    public RecentCounter() {
+        
+    }
+    
+    public int ping(int t) {
+        queue.offer(t);
+        while (queue.peek() < t - 3000) {
+            queue.poll();
+        }
+        return queue.size();
+    }
+}
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter obj = new RecentCounter();
+ * int param_1 = obj.ping(t);
+ */
