dd30a890c9997de328aa42b10fa159c6a38e9ebd
Kaiwen Sun <skw_kevin@126.com>
2019-08-24 18:58:52 -0700

    Update 1165.Single-Row Keyboard.java

--- 1165.Single-Row Keyboard.java
+++ 1165.Single-Row Keyboard.java
@@ -2,13 +2,12 @@ class Solution {
     public int calculateTime(String keyboard, String word) {
         Map<Integer, Integer> map = new HashMap<>();
         keyboard.chars().reduce(0, (index, c) -> {map.put(c, index); return index + 1;});
-        int lastChar = map.get((int)(word.charAt(0)));
-        List<Integer> lastCharWrapper = Arrays.asList(new Integer[]{lastChar});
+        List<Integer> lastCharWrapper = Arrays.asList(new Integer[]{0});
         return word.chars().reduce(
             0,
             (acc, c) -> {
-                acc += Math.abs(map.get(lastChar) - map.get(c));
-                lastCharWrapper.set(0, c);
+                acc += Math.abs(lastCharWrapper.get(0) - map.get(c));
+                lastCharWrapper.set(0, map.get(c));
                 return acc;
             }
         );
