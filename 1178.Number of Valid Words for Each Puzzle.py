b18063c9da26e96e10f4f1db2e5fdbb0e22d7eae
Kaiwen Sun <skw_kevin@126.com>
2019-09-01 16:39:07 -0700

    Create 1178.Number of Valid Words for Each Puzzle.py

---
+++ 1178.Number of Valid Words for Each Puzzle.py
@@ -0,0 +1,38 @@
+class Solution(object):
+    def findNumOfValidWords(self, words, puzzles):
+        """
+        :type words: List[str]
+        :type puzzles: List[str]
+        :rtype: List[int]
+        """
+        
+        def insertTrie(trie, word):
+            if len(word) <= 7:
+                for c in word:
+                    trie = trie[c]
+                trie['#'] = trie.get('#', 0) + 1
+
+        def query(trie, puzzle_list, head, index):
+            res = trie.get('#', 0) if head is None else 0
+            while index < len(puzzle_list):
+                c = puzzle_list[index]
+                if c in trie:
+                    inner_p = puzzle_list[index]
+                    c_missmatches = False
+                    while c > inner_p:
+                        index += 1
+                        if index == len(puzzle_list) or c < puzzle_list[index]:
+                            c_missmatches = True
+                            break
+                        inner_p = puzzle_list[index]
+                    if c_missmatches:
+                        continue
+                    res += query(trie[c], puzzle_list, None if c == head else head, index)
+                index += 1
+            return res
+
+        T = lambda: collections.defaultdict(T)
+        trie = T()
+        for word in words:
+            insertTrie(trie, sorted(set(word)))
+        return [query(trie, sorted(set(puzzle)), puzzle[0], 0) for puzzle in puzzles]
