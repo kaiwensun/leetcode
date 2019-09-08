e362d6df39ddec9405230b43283a592b9f255bc5
Kaiwen Sun <skw_kevin@126.com>
2019-09-07 23:10:15 -0700

    Create 1185.Day of the Week.py

---
+++ 1185.Day of the Week.py
@@ -0,0 +1,10 @@
+from datetime import date
+class Solution(object):
+    def dayOfTheWeek(self, day, month, year):
+        """
+        :type day: int
+        :type month: int
+        :type year: int
+        :rtype: str
+        """
+        return date(year, month, day).strftime("%A")
