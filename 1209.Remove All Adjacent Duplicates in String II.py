25a9c8ea795dcde85c25a8938d02c1ce6be2b5fa
Kaiwen Sun <skw_kevin@126.com>
2019-09-29 18:11:46 -0700

    Create 1209.Remove All Adjacent Duplicates in String II.py

---
+++ 1209.Remove All Adjacent Duplicates in String II.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def removeDuplicates(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: str
+        """
+        stack = []  # element: [letter, cnt]
+        for c in s:
+            if stack and stack[-1][0] == c:
+                stack[-1][1] += 1
+                if stack[-1][1] == k:
+                    stack.pop()
+            else:
+                stack.append([c, 1])
+        return ''.join(c * cnt for (c, cnt) in stack)
