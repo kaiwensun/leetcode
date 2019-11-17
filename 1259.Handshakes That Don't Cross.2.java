c9b59618a7d99d08c2defa7352e4455490d2639f
Kaiwen Sun <skw_kevin@126.com>
2019-11-17 03:54:47 -0800

    Update 1259.Handshakes That Don't Cross.2.java

--- 1259.Handshakes That Don't Cross.2.java
+++ 1259.Handshakes That Don't Cross.2.java
@@ -1,4 +1,5 @@
 // Improvement than v1: after spliting out leftSize, we dont have to split the rest into midSize and rightSize. The prod of dp[midSize] and dp[rightSize] is already found by dp.
+// Another hacky improvement is to make use of Catalan Numbers. See Python code at the end.
 
 import java.math.BigInteger; 
 
@@ -19,3 +20,13 @@ class Solution {
         return dp[dp.length - 1];
     }
 }
+
+
+/**
+    def numberOfWays(self, n):
+        res = 1
+        for i in xrange(1, n / 2 + 1):
+            res *= n - i + 1
+            res /= i
+        return res / (n / 2 + 1) % (10**9 + 7)
+ */
