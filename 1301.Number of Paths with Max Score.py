67597885e06e719c115d8ed2c05afccf6bdb7a00
Kaiwen Sun <skw_kevin@126.com>
2019-12-29 14:13:50 -0800

    Update 1301.Number of Paths with Max Score.py

--- 1301.Number of Paths with Max Score.py
+++ 1301.Number of Paths with Max Score.py
@@ -5,17 +5,16 @@ class Solution:
         @functools.lru_cache(None)
         def dp(i, j):
             if not (0 <= i < len(board) and 0 <= j < len(board[0]) and board[i][j] != "X"):
-                return 0, 0, False
+                return 0, 0
             if board[i][j] == "E":
-                return 0, 1, True
+                return 0, 1
             left = dp(i, j - 1)
             up = dp(i - 1, j)
             upleft = dp(i - 1, j - 1)
-            steps = tuple(filter(lambda step: step[2], (left, up, upleft)))
+            steps = (left, up, upleft)
             mx = max(tuple(map(lambda step: step[0], steps)) or [-1])
             mxsteps = tuple(filter(lambda step: step[0] == mx, steps))
-            if board[i][j] == "S":
-                return mx, sum(step[1] for step in mxsteps) % MOD, bool(mxsteps)
-            return mx + int(board[i][j]), sum(step[1] for step in mxsteps) % MOD, bool(mxsteps)
-        res = list(dp(len(board) - 1, len(board[0]) - 1))
-        return res[:2] if res[2] else [0, 0]
+            s = sum(step[1] for step in mxsteps)
+            cur = 0 if board[i][j] == "S" or s == 0 else int(board[i][j])
+            return mx + cur, sum(step[1] for step in mxsteps) % MOD
+        return list(dp(len(board) - 1, len(board[0]) - 1))
