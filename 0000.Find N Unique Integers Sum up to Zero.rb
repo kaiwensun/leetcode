69e8d36f5663449cdb8969726acb360f19208392
Kaiwen Sun <skw_kevin@126.com>
2019-12-29 16:05:41 -0800

    Update 0000.Find N Unique Integers Sum up to Zero.rb

--- 0000.Find N Unique Integers Sum up to Zero.rb
+++ 0000.Find N Unique Integers Sum up to Zero.rb
@@ -2,9 +2,5 @@
 # @return {Integer[]}
 def sum_zero(n)
     half = n / 2
-    if n.odd?
-        (1..half).to_a + (-half..-1).to_a + [0]
-    else
-        (1..half).to_a + (-half..-1).to_a
-    end
+    (1..half).to_a + (-half..-1).to_a + (n.odd? ? [0] : [])
 end
