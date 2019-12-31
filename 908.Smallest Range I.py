d876e50fde938b5b969d9dcf5e0db554d37eb5cc
Kaiwen Sun <skw_kevin@126.com>
2019-12-30 23:30:23 -0800

    Create 908.Smallest Range I.py

---
+++ 908.Smallest Range I.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def smallestRangeI(self, A, K):
+        """
+        :type A: List[int]
+        :type K: int
+        :rtype: int
+        """
+        return max(max(A) - min(A) - 2 * K, 0)
+        
