937862e9aa176cf5773b65de63061fdeb576c861
Kaiwen Sun <skw_kevin@126.com>
2020-01-04 20:12:24 -0800

    Update 1312.Minimum Insertion Steps to Make a String Palindrome.py

--- 1312.Minimum Insertion Steps to Make a String Palindrome.py
+++ 1312.Minimum Insertion Steps to Make a String Palindrome.py
@@ -3,9 +3,6 @@ class Solution:
     def minInsertions(self, s: str) -> int:
         @functools.lru_cache(None)
         def dp(i, j):
-            if j - i <= 1:
-                return 0
-            res = dp(i + 1, j - 1) if s[i] == s[j - 1] else float('inf')
-            res = min(res, dp(i + 1, j) + 1, dp(i, j - 1) + 1)
-            return res
+            if j - i <= 1: return 0
+            return dp(i + 1, j - 1) if s[i] == s[j - 1] else min(dp(i + 1, j) + 1, dp(i, j - 1) + 1)
         return dp(0, len(s))
