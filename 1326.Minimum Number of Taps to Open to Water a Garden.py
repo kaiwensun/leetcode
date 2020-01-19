8928fe0040c4359c56fe26538c792a7251c04e9d
Kaiwen Sun <skw_kevin@126.com>
2020-01-18 20:32:41 -0800

    Update 1326.Minimum Number of Taps to Open to Water a Garden.py

--- 1326.Minimum Number of Taps to Open to Water a Garden.py
+++ 1326.Minimum Number of Taps to Open to Water a Garden.py
@@ -1,21 +1,16 @@
 class Solution(object):
     def minTaps(self, n, ranges):
-        """
-        :type n: int
-        :type ranges: List[int]
-        :rtype: int
-        """
-        ranges = sorted([(max(i - r, 0), min(i + r, n)) for i, r in enumerate(ranges)])
-        res = [(0, 0), (0, 0)]
+        ranges = sorted([(i - r, min(i + r, n)) for i, r in enumerate(ranges)])
+        stack = [(0, 0), (0, 0)]
         for r in ranges:
-            if r[0] > res[-1][1]:
+            if r[0] > stack[-1][1]:
                 break
-            if r[1] <= res[-1][1]:
+            if r[1] <= stack[-1][1]:
                 continue
-            while r[0] <= res[-2][1] and len(res) > 2:
-                res.pop()
-            res.append(r)
-        if res[-1][1] >= n:
-            return len(res) - 2
+            while r[0] <= stack[-2][1] and len(stack) > 2:
+                stack.pop()
+            stack.append(r)
+        if stack[-1][1] == n:
+            return len(stack) - 2
         else:
             return -1
