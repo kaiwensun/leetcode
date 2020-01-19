7606341166f01ee79ebfc6290163b3b75abba4f4
Kaiwen Sun <skw_kevin@126.com>
2020-01-18 19:35:12 -0800

    1324.Print Words Vertically.py 2-linear

--- 1324.Print Words Vertically.py
+++ 1324.Print Words Vertically.py
@@ -10,3 +10,12 @@ class Solution(object):
             row = "".join(word[i:i+1] or ' ' for word in words)
             res.append(row.rstrip())
         return res
+
+    
+    """
+    # Shorter version
+    class Solution(object):
+        def printVertically(self, s):
+            words = s.split()
+            return ["".join(word[i:i+1] or ' ' for word in words).rstrip() for i in xrange(max(map(len, words)))]
+    """
