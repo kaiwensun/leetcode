33951e8e71bf782e0dd6e1838f38229c0d347ed8
Kaiwen Sun <skw_kevin@126.com>
2020-02-15 21:23:15 -0800

    Update 1353.Maximum Number of Events That Can Be Attended.py

--- 1353.Maximum Number of Events That Can Be Attended.py
+++ 1353.Maximum Number of Events That Can Be Attended.py
@@ -13,13 +13,20 @@ class Solution(object):
         cnt = 0
         event_id = 0
         while day <= total_days:
+		    # if no events are available to attend today, let time flies to the next available event.
             if event_id < len(events) and not min_heap:
-                day = max(day, events[event_id][0])
+                day = events[event_id][0]
+			
+			# all events starting from today are newly available. add them to the heap.
             while event_id < len(events) and events[event_id][0] <= day:
                 heapq.heappush(min_heap, events[event_id][1])
                 event_id += 1
+
+			# if the event at heap top already ended, then discard it.
             while min_heap and min_heap[0] < day:
                 heapq.heappop(min_heap)
+
+			# attend the event that will end the earliest
             if min_heap:
                 heapq.heappop(min_heap)
                 cnt += 1
