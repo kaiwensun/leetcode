c669c7ab2421497d1b56b7773c5f5ab099374642
Kaiwen Sun <skw_kevin@126.com>
2020-02-22 20:22:09 -0800

    Update 1362.Closest Divisors.py

--- 1362.Closest Divisors.py
+++ 1362.Closest Divisors.py
@@ -1,23 +1,13 @@
-import math
-class Solution(object):
-    def closestDivisors(self, num):
-        """
-        :type num: int
-        :rtype: List[int]
-        """
-
+class Solution:
+    def closestDivisors(self, num: int) -> List[int]:
+        
         num1 = num + 1
         num2 = num + 2
         mx = int(math.sqrt(num2))
-        res = [[], float('inf')]
-        
-        def candidate(a, b, res):
-            if b - a < res[1]:
-                res[0] = [a, b]
-                res[1] = b - a
-        for a in xrange(mx, 0, -1):
+
+        for a in range(mx, 0, -1):
             if num1 % a == 0:
-                candidate(a, num1 // a, res)
+                return [a, num1 // a]
             if num2 % a == 0:
-                candidate(a, num2 // a, res)
-        return res[0]
+                return [a, num2 // a]
+        assert(False)
