0a990b25eeb8969797411e408afb53dc04146e65
Kaiwen Sun <skw_kevin@126.com>
2020-03-21 21:48:04 -0700

    Create 1389.Create Target Array in the Given Order.py

---
+++ 1389.Create Target Array in the Given Order.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def createTargetArray(self, nums, index):
+        """
+        :type nums: List[int]
+        :type index: List[int]
+        :rtype: List[int]
+        """
+        target = []
+        for num, i in zip(nums, index):
+            target.insert(i, num)
+        return target
