1596f2ff2f2a0b1d7094ac34e3d4f03129e5f460
Kaiwen Sun <skw_kevin@126.com>
2020-04-18 21:33:10 -0700

    Create 1418.Display Table of Food Orders in a Restaurant.py

---
+++ 1418.Display Table of Food Orders in a Restaurant.py
@@ -0,0 +1,25 @@
+import collections
+class Solution(object):
+    def displayTable(self, orders):
+        foodNames = {}
+        title = ["Table"]
+        def getFoodId(foodName):
+            if foodName not in foodNames:
+                foodNames[foodName] = len(foodNames)
+                title.append(foodName)
+            return foodNames[foodName]
+        for order in orders:
+            foodNames[(order[2])] = None
+        for key in sorted(foodNames.keys()):
+            foodNames[key] = len(title) - 1
+            title.append(key)
+        res = collections.defaultdict(lambda: [0] * len(foodNames))
+        for order in orders:
+            res[order[1]][getFoodId(order[2])] += 1
+        table = [title]
+        for key in sorted(res.keys(), key=int):
+            value = map(str, res[key])
+            row = [key]
+            row.extend(value)
+            table.append(row)
+        return table
