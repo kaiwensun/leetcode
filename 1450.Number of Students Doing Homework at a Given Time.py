76d061c0623c6d00e4fc045da6ca8b69ef37f5ca
Kaiwen Sun <skw_kevin@126.com>
2020-05-16 20:20:16 -0700

    Create 1450.Number of Students Doing Homework at a Given Time.py

---
+++ 1450.Number of Students Doing Homework at a Given Time.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def busyStudent(self, startTime, endTime, queryTime):
+        """
+        :type startTime: List[int]
+        :type endTime: List[int]
+        :type queryTime: int
+        :rtype: int
+        """
+        return sum(s <= queryTime <= e for s, e in zip(startTime, endTime))
+            
