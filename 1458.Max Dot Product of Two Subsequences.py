b8661bd0d1b7416c968365221d4070f1b340c1a3
Kaiwen Sun <skw_kevin@126.com>
2020-05-23 20:56:51 -0700

    Create 1458.Max Dot Product of Two Subsequences.py

---
+++ 1458.Max Dot Product of Two Subsequences.py
@@ -0,0 +1,27 @@
+from functools import lru_cache
+class Solution:
+    def maxDotProduct(self, nums1: List[int], nums2: List[int]) -> int:
+        maxPrefix1 = [nums1[0]] * len(nums1)
+        maxPrefix2 = [nums2[0]] * len(nums2)
+        minPrefix1 = [nums1[0]] * len(nums1)
+        minPrefix2 = [nums2[0]] * len(nums2)
+        for i in range(1, len(nums1)):
+            maxPrefix1[i] = max(nums1[i], maxPrefix1[i - 1])
+            minPrefix1[i] = min(nums1[i], minPrefix1[i - 1])
+        for i in range(1, len(nums2)):
+            maxPrefix2[i] = max(nums2[i], maxPrefix2[i - 1])
+            minPrefix2[i] = min(nums2[i], minPrefix2[i - 1])
+        
+        @lru_cache(None)
+        def dp(i, j):
+            if i < 0 or j < 0:
+                return float("-inf")
+            if i == 0:
+                return max(nums1[0] * maxPrefix2[j], nums1[0] * minPrefix2[j])
+            if j == 0:
+                return max(nums2[0] * maxPrefix1[i], nums2[0] * minPrefix1[i])
+            res = dp(i - 1, j - 1)
+            res = max(nums1[i] * nums2[j], res, nums1[i] * nums2[j] + res)
+            res = max(res, dp(i, j - 1), dp(i - 1, j))
+            return res
+        return dp(len(nums1) - 1, len(nums2) - 1)
