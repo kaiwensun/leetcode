00cff10a44896d01adc1b98add02fd3037e609e6
Kaiwen Sun <skw_kevin@126.com>
2020-06-03 22:08:42 -0700

    Update 1029.Two City Scheduling.rb

--- 1029.Two City Scheduling.rb
+++ 1029.Two City Scheduling.rb
@@ -1,7 +1,7 @@
 # @param {Integer[][]} costs
 # @return {Integer}
 def two_city_sched_cost(costs)
-    indexes = costs.sort_by { |cost| cost[0] - cost[1] }
+    costs.sort_by { |cost| cost[0] - cost[1] }
         .each_with_index
         .map { |cost, index| index < costs.size / 2 ? cost[0] : cost[1] }
         .sum
