68fcf383d640adb726d5d34e281a01ff1ebfe4d3
Kaiwen Sun <skw_kevin@126.com>
2020-06-14 17:58:57 -0700

    Create 1475.Final Prices With a Special Discount in a Shop.rb

---
+++ 1475.Final Prices With a Special Discount in a Shop.rb
@@ -0,0 +1,13 @@
+# @param {Integer[]} prices
+# @return {Integer[]}
+def final_prices(prices)
+    stack = [0]
+    res = []
+    mn_index = -1
+    for price in prices.reverse
+        stack.pop while stack[-1] > price
+        res << [price - stack.last, 0].max
+        stack << price if price != stack.last
+    end
+    res.reverse
+end
