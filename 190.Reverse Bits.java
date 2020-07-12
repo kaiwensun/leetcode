853f6419a11a02e26254454af64fd2002b4ed9f0
Kaiwen Sun <skw_kevin@126.com>
2020-07-12 00:18:47 -0700

    Create 190.Reverse Bits.java

---
+++ 190.Reverse Bits.java
@@ -0,0 +1,12 @@
+public class Solution {
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+        int res = 0;
+        for (int i = 0; i < 32; i++) {
+            res <<= 1;
+            res |= n & 1;
+            n >>= 1;
+        }
+        return res;
+    }
+}
