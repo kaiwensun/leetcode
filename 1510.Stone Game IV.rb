cc400ae9256ed06e1e24950cf7a4aa36329bf024
Kaiwen Sun <skw_kevin@126.com>
2020-07-12 01:43:47 -0700

    Update 1510.Stone Game IV.rb

--- 1510.Stone Game IV.rb
+++ 1510.Stone Game IV.rb
@@ -1,25 +1,25 @@
 # @param {Integer} n
 # @return {Boolean}
+
+$dp = [false]
 def winner_square_game(n)
-    dp = Array.new(n + 1)
-    dp[0] = false
-    win(n, dp)
+    win(n)
 end
 
-def win(n, dp)
-    if dp[n].nil?
-        dp[n] = false
+def win(n)
+    if $dp[n].nil?
+        $dp[n] = false
         i = 1
         prev = n - i * i
         while prev >= 0
-            dp[prev] = win(prev, dp)
-            if !dp[prev]
-                dp[n] = true
+            $dp[prev] = win(prev)
+            if !$dp[prev]
+                $dp[n] = true
                 break
             end
             i += 1
             prev = n - i * i
         end
     end
-    dp[n]
+    $dp[n]
 end
