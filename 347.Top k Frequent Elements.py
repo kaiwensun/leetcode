c87e8dd3845cb0c8e474108af1fff81a217edde4
Kaiwen Sun <skw_kevin@126.com>
2020-07-17 15:05:39 -0700

    Create 347.Top k Frequent Elements.py

---
+++ 347.Top k Frequent Elements.py
@@ -0,0 +1,9 @@
+from collections import Counter
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        return map(lambda (num, _): num, Counter(nums).most_common(k))
