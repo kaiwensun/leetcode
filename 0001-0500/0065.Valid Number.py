5b959c3143c8df2b800d428ec88eb62868a0cce3
Kaiwen Sun <skw_kevin@126.com>
2020-09-06 01:33:48 -0700

    65.Valid Number

---
+++ 0001-0500/0065.Valid Number.py
@@ -0,0 +1,46 @@
+from collections import Counter
+class Solution:
+    def isNumber(self, s: str) -> bool:
+        def isUnsignedInt(s):
+            for c in s:
+                if not '0' <= c <= '9':
+                    return False
+            return len(s) != 0
+
+        def isFloat(s):
+            cnt = Counter(s)
+            if cnt["."] == 1:
+                return all(map(isUnsignedInt, s.split(".")))
+            return False
+        
+        def isFreeSideFloat(s):
+            if not s:
+                return False
+            if s[0] == ".":
+                s = s[1:]
+            elif s[-1] == ".":
+                s = s[:-1]
+            return isUnsignedInt(s)
+
+        def removeSign(s):
+            if s and s[0] in "+-":
+                return s[1:]
+            return s
+
+        s = s.strip()
+        cnt = Counter(s)
+        if cnt["e"] == 0:
+            s = removeSign(s)
+            return isUnsignedInt(s) or isFreeSideFloat(s) or isFloat(s)
+        elif cnt["e"] == 1:
+            dec, exp = s.split("e")
+            dec = removeSign(dec)
+            if not (isUnsignedInt(dec) or isFreeSideFloat(dec) or isFloat(dec)):
+                return False
+            exp = removeSign(exp)
+            if not isUnsignedInt(exp):
+                return False
+        else:
+            return False
+        return True
+
