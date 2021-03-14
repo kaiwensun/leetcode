09036837b98703b2ae31388a6b76b249f174635d
Kaiwen Sun <skw_kevin@126.com>
2021-03-13 19:48:18 -0800

    improve 5704

--- 5704.py
+++ 5704.py
@@ -6,13 +6,15 @@ class Solution(object):
         :rtype: int
         """
         i = j = k
+        n = len(nums)
+        nums.append(float("-inf"))
         mn = res = nums[k]
-        while i >= 0 or j < len(nums):
+        while i >= 0 or j < n:
             while i >= 0 and nums[i] >= mn:
                 i -= 1
-            while j < len(nums) and nums[j] >= mn:
+            while j < n and nums[j] >= mn:
                 j += 1
             res = max(res, mn * (j - i - 1))
-            mn = max(float("-inf") if i < 0 else nums[i], float("-inf") if j >= len(nums) else nums[j])
+            mn = max(nums[i], nums[j])
         return res
 
