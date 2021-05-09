888b9f3ebdf78bd1cb03c4fcc3b20aec8670f1fe
Kaiwen Sun <skw_kevin@126.com>
2021-05-08 21:37:05 -0700

    optimize 1855

--- 1501-2000/1855.Maximum Distance Between a Pair of Values.py
+++ 1501-2000/1855.Maximum Distance Between a Pair of Values.py
@@ -1,15 +1,12 @@
-import heapq
-
 class Solution:
     def maxDistance(self, nums1: List[int], nums2: List[int]) -> int:
-        heap = [(-num, index) for index, num in enumerate(nums2)]
-        heapq.heapify(heap)
+        sorted_nums2 = [(num, index) for index, num in enumerate(nums2)]
+        sorted_nums2.sort()
         res = 0
-        print(heap)
         for i in range(len(nums1)):
-            while heap and nums1[i] <= -heap[0][0]:
-                if i <= heap[0][1]:
-                    res = max(res, heap[0][1] - i)
-                heapq.heappop(heap)
+            while sorted_nums2 and nums1[i] <= sorted_nums2[-1][0]:
+                if i <= sorted_nums2[-1][1]:
+                    res = max(res, sorted_nums2[-1][1] - i)
+                sorted_nums2.pop()
         return res
 
