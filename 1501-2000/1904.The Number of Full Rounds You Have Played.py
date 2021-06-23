19b202debe52c735097c907798aa00873b713450
Kaiwen Sun <skw_kevin@126.com>
2021-06-22 18:57:16 -0700

    correct 1904

--- 1501-2000/1904.The Number of Full Rounds You Have Played.py
+++ 1501-2000/1904.The Number of Full Rounds You Have Played.py
@@ -5,6 +5,7 @@ class Solution:
         s = datetime.strptime(startTime, "%H:%M")
         e = datetime.strptime(finishTime, "%H:%M")
         ROUND = 15 * 60
-        return int(e.timestamp() // ROUND - (s.timestamp() + ROUND - 1) // ROUND) % (24 * 4)
-
+        if 0 <= (e - s).seconds < ROUND:
+            return 0
+        return int(e.timestamp() // ROUND - (s.timestamp() + ROUND - 60) // ROUND) % (24 * 4)
 
--- 1501-2000/1904.The Number of Full Rounds You Have Played.ts
+++ 1501-2000/1904.The Number of Full Rounds You Have Played.ts
@@ -1,6 +1,13 @@
 function numberOfRounds(startTime: string, finishTime: string): number {
         const getMinute = (time:string):number => parseInt(time.slice(0, 2)) * 60 + parseInt(time.slice(3));
         const ROUND = 15;
-        return (Math.floor(getMinute(finishTime) / ROUND) - Math.ceil(getMinute(startTime) / ROUND) + 24 * 4) % (24 * 4);
+        const start = getMinute(startTime);
+        const end = getMinute(finishTime);
+        const startRound = Math.ceil(start / ROUND);
+        const endRound = Math.floor(end / ROUND);
+        if (startRound === endRound + 1 && start <= end) {
+            return 0;
+        }
+        return (Math.floor(end / ROUND) - Math.ceil(start / ROUND) + 24 * 4) % (24 * 4);
 };
 
