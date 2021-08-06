6e423881af7d7ab9993a99f5619b0354fa38b5c9
Kaiwen Sun <skw_kevin@126.com>
2021-08-05 23:25:56 -0700

    improve 0847.py

--- 0501-1000/0847.Shortest Path Visiting All Nodes.py
+++ 0501-1000/0847.Shortest Path Visiting All Nodes.py
@@ -1,17 +1,23 @@
 class Solution:
     def shortestPathLength(self, graph: List[List[int]]) -> int:
         n = len(graph)
-        res = float("inf")
-        seen = defaultdict(lambda: float("inf"))
-        for start in range(n):
-            queue = deque([(1 << start, start, 0)])
-            while queue:
-                visited, last, dist = queue.popleft()
-                if visited == (1 << n) - 1:
-                    res = min(res, dist)
-                if seen[(visited, last)] > dist:
-                    seen[(visited, last)] = dist
-                    for nxt in graph[last]:
-                        queue.append((visited | (1 << nxt), nxt, dist + 1))
-        return res
+        if n == 1:
+            return 0
+        res = 0
+        seen = set()
+        queue = deque([(1 << start, start) for start in range(n)])
+        queue.append(("#", "#"))
+        while True:
+            visited, last = queue.popleft()
+            if visited == "#":
+                res += 1
+                queue.append(("#", "#"))
+                continue
+            for nxt in graph[last]:
+                key = (visited | (1 << nxt), nxt)
+                if key[0] == (1 << n) - 1:
+                    return res + 1
+                if key not in seen:
+                    seen.add(key)
+                    queue.append(key)
 
