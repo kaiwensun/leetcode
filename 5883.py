720360fb961eb695261fcb6184f544782d084a42
Kaiwen Sun <skw_kevin@126.com>
2021-09-25 21:17:50 -0700

    simplify 5883.py

--- 5883.py
+++ 5883.py
@@ -1,36 +1,31 @@
+DELTAS = ((0, 1), (0, -1), (1, 0), (-1, 0))
+
 class Solution:
     def placeWordInCrossword(self, board: List[List[str]], word: str) -> bool:
         m, n = len(board), len(board[0])
 
+        def inboard(i, j):
+            return 0 <= i < m and 0 <= j < n
+
         def check(i, j, di, dj):
             endi, endj = i + di * (len(word) - 1), j + dj * (len(word) - 1)
-            if not (0 <= endi < m and 0 <= endj < n):
+            if not inboard(endi, endj):
                 return False
             for k in range(len(word)):
-                if board[i][j] != " ":
-                    if board[i][j] != word[k]:
-                        return False
+                if board[i][j] != " " and board[i][j] != word[k]:
+                    return False
                 i += di
                 j += dj
-            if (not (0 <= i < m and 0 <= j < n)) or board[i][j] == "#":
-                return True
-            return False
+            return not inboard(i, j) or board[i][j] == "#"
 
         for i in range(len(board)):
             for j in range(len(board[i])):
                 if board[i][j] == "#":
                     continue
-                deltas = []
-                if i == 0 or board[i - 1][j] == "#":
-                    deltas.append((1, 0))
-                if i == m - 1 or board[i + 1][j] == "#":
-                    deltas.append((-1, 0))
-                if j == 0 or board[i][j - 1] == "#":
-                    deltas.append((0, 1))
-                if j == n - 1 or board[i][j + 1] == "#":
-                    deltas.append((0, -1))
-                for di, dj in deltas:
-                    if check(i, j, di, dj):
-                        return True
+                for di, dj in DELTAS:
+                    x, y = i - di, j - dj
+                    if not inboard(x, y) or board[x][y] == "#":
+                        if check(i, j, di, dj):
+                            return True
         return False
 
