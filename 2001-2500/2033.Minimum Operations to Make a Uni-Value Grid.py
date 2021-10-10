0edeb48ce2814b9e9f9395bd89d2ee0202a7f799
Kaiwen Sun <skw_kevin@126.com>
2021-10-09 22:08:35 -0700

    remove ugly solution of 2033

--- 2001-2500/2033.Minimum Operations to Make a Uni-Value Grid.py
+++ 2001-2500/2033.Minimum Operations to Make a Uni-Value Grid.py
@@ -1,29 +1,16 @@
-from collections import Counter
-
-class Solution:
-    def minOperations(self, grid: List[List[int]], x: int) -> int:
-        cnt = Counter()
+class Solution(object):
+    def minOperations(self, grid, x):
+        """
+        :type grid: List[List[int]]
+        :type x: int
+        :rtype: int
+        """
+        nums = []
         for row in grid:
-            for num in row:
-                cnt[num] += 1
-        stats = sorted(cnt.items())
-        lcost = rcost = 0
-        base = stats[0][0]
-        for num, c in stats:
-            if (num - base) % x != 0:
-                return -1
-            rcost += c * (num - base) // x
-        res = rcost
-        lsize, rsize = 0, len(grid) * len(grid[0])
-        i = 0
-        cost = float("inf")
-        for target in range(base, stats[-1][0] + x, x):
-            cost = min(cost, lcost + rcost)
-            if stats[i][0] == target:
-                rsize -= stats[i][1]
-                lsize += stats[i][1]
-                i += 1
-            lcost += lsize
-            rcost -= rsize
-        return cost
+            nums.extend(row)
+        if any((nums[i] - nums[i - 1]) % x for i in range(1, len(nums))):
+            return -1
+        nums.sort()
+        medium = nums[len(nums) // 2]
+        return sum(abs(num - medium) for num in nums) // x
 
