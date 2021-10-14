8a2ce3f6449924e5df976104baa16cc62d93e459
Kaiwen Sun <skw_kevin@126.com>
2021-10-14 10:19:49 -0700

    144.ts

---
+++ 0001-0500/0144.Binary Tree Preorder Traversal.ts
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function preorderTraversal(root: TreeNode | null): number[] {
+    const res: number[] = [];
+    const stack: TreeNode[] = root ? [root] : [];
+
+    while (stack.length) {
+        const cur = stack.pop();
+        res.push(cur.val);
+        cur.right && stack.push(cur.right);
+        cur.left && stack.push(cur.left);
+    }
+    return res;
+};
+
