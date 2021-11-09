37e1e9d24089d35c8c735fabad64f6dcec802b58
Kaiwen Sun <skw_kevin@126.com>
2021-11-09 00:08:53 -0800

    refactor 488.py

--- 0001-0500/0488.Zuma Game.py
+++ 0001-0500/0488.Zuma Game.py
@@ -1,50 +1,41 @@
-class Solution(object):
-    def findMinStep(self, board, hand):
-        """
-        :type board: str
-        :type hand: str
-        type: int
-        """
-        self.cache = {}
-        cleaner = re.compile("(.)\\1{2,}")
-        def cleanUp(board):
-            new_board = cleaner.sub('', board)
-            while True:
-                new_board = cleaner.sub('', board)
-                if new_board == board:
-                    break
-                board = new_board
+from functools import cache
+from collections import Counter
+
+class Solution:
+    def findMinStep(self, board: str, hand: str) -> int:
+        
+        cnt = Counter(hand)
+        remain_hand = tuple(cnt[c] for c in "RYBGW")
+        board = tuple("RYBGW".index(c) for c in board)
+
+        def clean(board, i):
+            if not 0 <= i < len(board):
+                return board
+            l = r = i
+            while l >=0 and board[l] == board[i]:
+                l -= 1
+            while r < len(board) and board[r] == board[i]:
+                r += 1
+            if r - l > 3:
+                board = board[:l + 1] + board[r:]
+                return clean(board, l)
             return board
 
-        def helper(board, hand):
+        @cache
+        def dfs(board, remain_hand):
             if not board:
-                return sum(hand.values())
-            hash_key = (board, tuple(sorted(hand.iteritems())))
-            if hash_key in self.cache:
-                return self.cache[hash_key]
-            rval = -1
-            i = 0
-            while i < len(board):
-                key = board[i]
-                has = collections.Counter(board[i:i + 3])[key]
-                if has == 2 and board[i + 1] != board[i]:
-                    has = 1
-                needs = 3 - has
-                if needs <= hand[key]:
-                    hand[key] -= needs
-                    new_board = board[:i] + board[i + has:]
-                    new_board = cleanUp(new_board)
-                    rval = max(rval, helper(new_board, hand))
-                    hand[key] += needs
-                    i += has
-                else:
-                    i += 1
-            self.cache[hash_key] = rval
-            return rval
-                        
-        hand_counter = collections.Counter(hand)
-        rval = helper(board, hand_counter)
-        if rval == -1:
-            return -1
-        return len(hand) - rval
+                return sum(remain_hand)
+            res = float("-inf")
+            remain_hand = list(remain_hand)
+            for j in range(len(remain_hand)):
+                if remain_hand[j] == 0:
+                    continue
+                for i in range(len(board) + 1):
+                    remain_hand[j] -= 1
+                    res = max(res, dfs(clean(board[:i] + (j,) + board[i:], i), tuple(remain_hand)))
+                    remain_hand[j] += 1
+            return res
+
+        res = dfs(board, remain_hand)
+        return sum(remain_hand) - res if res != float("-inf") else -1
 
