d142d835e35c83cdebc84060044de2cc35c6dc11
Kaiwen Sun <skw_kevin@126.com>
2021-12-27 18:07:48 -0800

    improve 472.py

--- 0001-0500/0472.Concatenated Words.py
+++ 0001-0500/0472.Concatenated Words.py
@@ -5,24 +5,30 @@ class Solution(object):
         :type words: List[str]
         :rtype: List[str]
         """
-        T = lambda: collections.defaultdict(T)
-        trie = T()
-        res = []
-        for word in words:
-            if word:
-                p = trie
-                for c in word:
-                    p = p[c]
-                p["#"] = word
-            
+
         def dfs(p, word, i):
             if i == len(word):
-                return "#" in p and p["#"] != word
+                return "#" in p
             if "#" in p and dfs(trie, word, i):
                 return True
             c = word[i]
             if c in p and dfs(p[c], word, i + 1):
                 return True
             return False
+
+        T = lambda: collections.defaultdict(T)
+        trie = T()
         res = []
-        return [word for word in words if dfs(trie, word, 0)]
+        words.sort(key=len)
+        for word in words:
+            if not word:
+                continue
+            if dfs(trie, word, 0):
+                res.append(word)
+            else:
+                p = trie
+                for c in word:
+                    p = p[c]
+                p["#"] = True
+        return res
+
