00f6978ea7583195a9780969f4208886c15cfb00
Kaiwen Sun <skw_kevin@126.com>
2022-02-05 21:28:39 -0800

    hashset solution for 2166

---
+++ 2001-2500/2166.Design Bitset.2.(HashSet).java
@@ -0,0 +1,64 @@
+class Bitset {
+    private Set<Integer> set;
+    private int size;
+    private boolean flipped;
+
+    public Bitset(int size) {
+        set = new HashSet<>();
+        this.size = size;
+        flipped = false;
+    }
+    
+    public void fix(int idx) {
+        if (flipped) {
+            set.remove(idx);
+        } else {
+            set.add(idx);
+        }
+    }
+    
+    public void unfix(int idx) {
+        if (flipped) {
+            set.add(idx);
+        } else {
+            set.remove(idx);
+        }
+    }
+    
+    public void flip() {
+        flipped = !flipped;
+    }
+    
+    public boolean all() {
+        return set.size() == (flipped ? 0 : size);
+    }
+    
+    public boolean one() {
+        return flipped ? set.size() < size : !set.isEmpty();
+    }
+    
+    public int count() {
+        return flipped ? size - set.size() : set.size();
+    }
+    
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < size; i++) {
+            sb.append(set.contains(i) != flipped ? 1 : 0);
+        }
+        return sb.toString();
+    }
+}
+
+/**
+ * Your Bitset object will be instantiated and called as such:
+ * Bitset obj = new Bitset(size);
+ * obj.fix(idx);
+ * obj.unfix(idx);
+ * obj.flip();
+ * boolean param_4 = obj.all();
+ * boolean param_5 = obj.one();
+ * int param_6 = obj.count();
+ * String param_7 = obj.toString();
+ */
+
