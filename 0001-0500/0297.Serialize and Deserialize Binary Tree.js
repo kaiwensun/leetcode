f8292215b2e4c310c93fe73f82a449d271a26a04
Kaiwen Sun <skw_kevin@126.com>
2022-03-20 15:09:36 -0700

    297.js

---
+++ 0001-0500/0297.Serialize and Deserialize Binary Tree.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+
+/**
+ * Encodes a tree to a single string.
+ *
+ * @param {TreeNode} root
+ * @return {string}
+ */
+var serialize = JSON.stringify;
+
+/**
+ * Decodes your encoded data to tree.
+ *
+ * @param {string} data
+ * @return {TreeNode}
+ */
+var deserialize = JSON.parse;
+
+/**
+ * Your functions will be called as such:
+ * deserialize(serialize(root));
+ */
+
