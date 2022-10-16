744785d945827b912d345187b2a9f9cd2924c059
Kaiwen Sun <skw_kevin@126.com>
2022-10-15 21:08:30 -0700

    improve 6207.ts

--- 6207.ts
+++ 6207.ts
@@ -1,24 +1,15 @@
 function countSubarrays(nums: number[], minK: number, maxK: number): number {
     if (minK > maxK) return 0;
-    let res = 0;
-    nums.push(Infinity);
-    let start = 0;
+    let res = 0, start = 0;
+    let minInd = -1, maxInd = -1;
     nums.forEach((num, end) => {
         if (num > maxK || num < minK) {
-            if (start < end) {
-                if (minK === maxK) {
-                    res +=  (1 + end - start) * (end - start) / 2;
-                } else {
-                    let minInd = start - 1, maxInd = start - 1;
-                    for (let i = start; i < end; i++) {
-                        if (nums[i] === minK) minInd = i;
-                        if (nums[i] === maxK) maxInd = i;
-                        res += Math.min(maxInd, minInd) - start + 1;
-                    }
-                }
-            }
             start = end + 1;
+            minInd = maxInd = end;
         }
+        if (nums[end] === minK) minInd = end;
+        if (nums[end] === maxK) maxInd = end;
+        res += Math.min(maxInd, minInd) - start + 1;
     });
     return res;
 };
